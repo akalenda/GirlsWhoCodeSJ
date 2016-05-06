895fc57bca1baad473453284e5c57ebeb6b32b7a
akalenda <akalenda@users.noreply.github.com>
2016-05-06 13:50:40 -0700

    Commented explaining listeners

--- webpageProject/catBlink.js
+++ webpageProject/catBlink.js
@@ -2,6 +2,12 @@ var catOpen = document.getElementById("catOpen");
 var catClosed = document.getElementById("catClosed");
 var DEFAULT_DISPLAY = catOpen.style.display;
 
+/* A "listener" is a function that you create and give to the program.
+ It "listens" for an event of your choosing that happen on an HTML
+ element that you choose. For example, here we listen for a "mousedown"
+ event on the image with the cat's eyes open, and when that event happens
+ the function to close the cat's eyes fires.
+ */
 catOpen.addEventListener("mousedown", closeTheCatsEyes);
 catClosed.addEventListener("mouseup", openTheCatsEyes);
 
