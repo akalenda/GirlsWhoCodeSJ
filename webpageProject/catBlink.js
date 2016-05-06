8d582c2f5988f6d41f8436947b1ad6fcef7af464
akalenda <akalenda@users.noreply.github.com>
2016-05-05 18:38:42 -0700

    Reformatting w/ IntelliJ

--- webpageProject/catBlink.js
+++ webpageProject/catBlink.js
@@ -5,15 +5,15 @@ var DEFAULT_DISPLAY = catOpen.style.display;
 catOpen.addEventListener("mousedown", closeTheCatsEyes);
 catClosed.addEventListener("mouseup", openTheCatsEyes);
 
-function closeTheCatsEyes(){
-	// make the catOpen display disappear
-	catOpen.style.display = 'none';
-	
-	// make the catClosed appear
-	catClosed.style.display = DEFAULT_DISPLAY;
+function closeTheCatsEyes() {
+    // make the catOpen display disappear
+    catOpen.style.display = 'none';
+
+    // make the catClosed appear
+    catClosed.style.display = DEFAULT_DISPLAY;
 }
 
-function openTheCatsEyes(){
-	catOpen.style.display = DEFAULT_DISPLAY;
-	catClosed.style.display = 'none';
+function openTheCatsEyes() {
+    catOpen.style.display = DEFAULT_DISPLAY;
+    catClosed.style.display = 'none';
 }
--- webpageProject/rings.js
+++ webpageProject/rings.js
@@ -34,37 +34,37 @@ canvasElement.addEventListener('mousedown', drawManyCircles, false);
  * the event -- the mouse click -- to this function. So I'm going to call
  * that variable "event".
  */
-function drawManyCircles(event){
-	/* The event object has all sorts of useful information
-	   about what happened. What I'm interested in is where the
-	   user clicked. So I'm going to extract the X-Y coordinates.
-	*/
-	var x = event.pageX - canvasElement.offsetLeft;
-	var y = event.pageY - canvasElement.offsetTop;
-	/* Something you might notice is that the coordinates it
-	   gives are for the ENTIRE webpage: pageX, pageY. We want 
-	   the coordinates to be relative to the top-left corner
-	   of our canvas, which is why I subtract those offsets.
-	*/
-	/* Next, I'm going to draw the circles, centered on
-	   the point of the mouse click.
-	   
-	   It's going to be a lot of circles, so we want to
-	   use a loop.
-	   
-	   I could set up the loop so that it creates a certain
-	   number of circles. Maybe 10 circles. Then it would look like:
-	   
-	   for (var count = 0; count <= 10; count += 1)
-		   
-	   But I want it to draw circles until they are a certain size.
-	   So I use "r" for radius, start it at radius 1, and go until the
-	   radius is over 100, in increments of 5:
-	*/
-	for (var r = 1; r <= 100; r += 5) {
-		var delay = r * 2;
-		drawCircle(canvasElement, x, y, r, delay);
-	}
+function drawManyCircles(event) {
+    /* The event object has all sorts of useful information
+     about what happened. What I'm interested in is where the
+     user clicked. So I'm going to extract the X-Y coordinates.
+     */
+    var x = event.pageX - canvasElement.offsetLeft;
+    var y = event.pageY - canvasElement.offsetTop;
+    /* Something you might notice is that the coordinates it
+     gives are for the ENTIRE webpage: pageX, pageY. We want
+     the coordinates to be relative to the top-left corner
+     of our canvas, which is why I subtract those offsets.
+     */
+    /* Next, I'm going to draw the circles, centered on
+     the point of the mouse click.
+
+     It's going to be a lot of circles, so we want to
+     use a loop.
+
+     I could set up the loop so that it creates a certain
+     number of circles. Maybe 10 circles. Then it would look like:
+
+     for (var count = 0; count <= 10; count += 1)
+
+     But I want it to draw circles until they are a certain size.
+     So I use "r" for radius, start it at radius 1, and go until the
+     radius is over 100, in increments of 5:
+     */
+    for (var r = 1; r <= 100; r += 5) {
+        var delay = r * 2;
+        drawCircle(canvasElement, x, y, r, delay);
+    }
 }
 
 /**
@@ -80,41 +80,41 @@ function drawManyCircles(event){
  * @param radius {Number} - How far from its center the arc of the circle will be drawn
  * @param delayInMilliseconds {Number} - How long to wait before drawing the circle
  */
-function drawCircle(canvas, x, y, radius, delayInMilliseconds){
+function drawCircle(canvas, x, y, radius, delayInMilliseconds) {
     /* I want it to be animated, so that the circles don't appear all
-       at once. So I set a timeout, using the delay I passed in as an
-       argument. After the milliseconds have been passed, the function
-       I give it will be called.
+     at once. So I set a timeout, using the delay I passed in as an
+     argument. After the milliseconds have been passed, the function
+     I give it will be called.
      */
     setTimeout(seriouslyDrawThatCircleNow, delayInMilliseconds);
 
     /* And here I create the function.
      */
-    function seriouslyDrawThatCircleNow(){
+    function seriouslyDrawThatCircleNow() {
         /* I start by getting the context of the canvas.
-           This is an object that has information about
-           how and where to draw... don't worry about it too much.
+         This is an object that has information about
+         how and where to draw... don't worry about it too much.
          */
         var context = canvas.getContext("2d");
         /* I start a new path. This is a math term; lines and
-           curves together make a "path".
+         curves together make a "path".
          */
         context.beginPath();
         /* Now I draw the curve of the circle. It's a perfect arc,
-           centered at the x-y coordinates, and with the radius I
-           give. It starts at 0 radians, and continues around to
-           2-pi radians, making a complete circle.
+         centered at the x-y coordinates, and with the radius I
+         give. It starts at 0 radians, and continues around to
+         2-pi radians, making a complete circle.
          */
         context.arc(x, y, radius, 0, 2 * Math.PI, true);
         /* The path won't show up if the line width stays at zero.
          */
         context.lineWidth = 1;
         /* And if there's no color, it won't show up no matter
-           how wide it is.
+         how wide it is.
          */
         context.strokeStyle = 'black';
         /* The path is ready! Now we apply the stroke, and it draws
-           out the path we described.
+         out the path we described.
          */
         context.stroke();
     }
