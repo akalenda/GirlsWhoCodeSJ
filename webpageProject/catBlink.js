386fa5d90400abda4b6649be23203cf9cb99eb12
akalenda <akalenda@users.noreply.github.com>
2016-05-05 18:34:28 -0700

    Added cat-blinking

---
+++ webpageProject/catBlink.js
@@ -0,0 +1,19 @@
+var catOpen = document.getElementById("catOpen");
+var catClosed = document.getElementById("catClosed");
+var DEFAULT_DISPLAY = catOpen.style.display;
+
+catOpen.addEventListener("mousedown", closeTheCatsEyes);
+catClosed.addEventListener("mouseup", openTheCatsEyes);
+
+function closeTheCatsEyes(){
+	// make the catOpen display disappear
+	catOpen.style.display = 'none';
+	
+	// make the catClosed appear
+	catClosed.style.display = DEFAULT_DISPLAY;
+}
+
+function openTheCatsEyes(){
+	catOpen.style.display = DEFAULT_DISPLAY;
+	catClosed.style.display = 'none';
+}
